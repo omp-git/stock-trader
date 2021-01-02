b031114159397e9ba85ac2e229dce4cd1d876c46
Ehsan Mohammadpour <omp.mohammadpour@gmail.com>
2021-01-02 19:53:30 +0330

    customize bootstrap sass

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -25,4 +25,13 @@ export default {
   color: #2c3e50;
   margin-top: 150px;
 }
+
+.hello {
+
+    span {
+        text-decoration: underline;
+    }
+
+}
+
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1 class="text-info">{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.<br>
-      <span class="text-info">(Test Bootstrap Sass)</span>
+      <span class="text-danger">(Test Bootstrap Sass)</span>
     </p>
   </div>
 </template>
